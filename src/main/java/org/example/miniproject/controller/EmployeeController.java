2cfe749f099fb4190e031a4bf6130aaf0c96457a
letrungchien <letrungchien195@gmail.com>
2026-05-02 20:55:48 +0700

    update

--- src/main/java/org/example/miniproject/controller/EmployeeController.java
+++ src/main/java/org/example/miniproject/controller/EmployeeController.java
@@ -4,6 +4,10 @@ import org.example.miniproject.model.Employee;
 import org.example.miniproject.repository.DepartmentRepository;
 import org.example.miniproject.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -21,8 +25,39 @@ public class EmployeeController {
     @Autowired
     private DepartmentRepository departmentRepository;
     @GetMapping
-    public String home(Model model) {
-        model.addAttribute("employees", employeeRepository.findAll());
+    public String home(
+            Model model,
+            @RequestParam(defaultValue = "") String keyword,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "name") String sortField,
+            @RequestParam(defaultValue = "asc") String sortDir
+    ) {
+
+        int size = 5;
+
+        Sort sort = sortDir.equals("asc")
+                ? Sort.by(sortField).ascending()
+                : Sort.by(sortField).descending();
+
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        Page<Employee> employeePage;
+
+        if (keyword != null && !keyword.isEmpty()) {
+            employeePage = employeeRepository.findByNameContainingIgnoreCase(keyword, pageable);
+        } else {
+            employeePage = employeeRepository.findAll(pageable);
+        }
+
+        model.addAttribute("employees", employeePage.getContent());
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", employeePage.getTotalPages());
+        model.addAttribute("totalItems", employeePage.getTotalElements());
+
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+
         return "home";
     }
 
--- src/main/java/org/example/miniproject/repository/EmployeeRepository.java
+++ src/main/java/org/example/miniproject/repository/EmployeeRepository.java
@@ -1,7 +1,11 @@
 package org.example.miniproject.repository;
 
 import org.example.miniproject.model.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Page<Employee> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
